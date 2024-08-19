b3d08c1486ac80b27aff9a919758a486f4eb5521
fantasma1234 <guilhermedroni07@gmail.com>
2024-08-19 00:09:31 -0300

    Tradução em pt-br

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let cod = document.querySelector(".cod")
-let frase = document.querySelector(".frase")
+let frase = document.querySelector(".frase.En")
+let frasePt = document.querySelector(".frase.Pt")
 let botao = document.querySelector(".btn")
 
 const url = "https://api.adviceslip.com/advice"
@@ -9,7 +10,15 @@ async function getAdvice() {
     const {slip: {id, advice}} = await res.json()
     cod.innerText = id
     frase.innerText = advice
+    frase.value = advice
+    traduzir()
+}
 
+async function traduzir() {
+    await fetch (
+        `https://api.mymemory.translated.net/get?q=${frase.value}&langpair=en|pt-BR`
+    ).then((res) => res.json())
+    .then((data) => {frasePt.innerText = data.responseData.translatedText})
 }
 
 getAdvice()
